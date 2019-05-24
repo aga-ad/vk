5d1833fcbbb0fbe47cc11f9163f3c8956fc6a032
Artur Aganov <aganov.a.d@gmail.com>
2019-05-24 04:01:47 +0300

    done

--- core_ml/main.cpp
+++ core_ml/main.cpp
@@ -1,9 +1,9 @@
+
 #include <vector>
 #include <iostream>
 #include <algorithm>
 #include <chrono>
-#include <Python.h>
-#include "Numeric/arrayobject.h"
+#include <random>
 
 using namespace std;
 
@@ -12,17 +12,26 @@ typedef double num;
 
 // Содержит индексы двух соседних кластеров
 struct Neighbour {
-    size_t l, r;
+    size_t left, right;
 };
 
-// l, r - индексы двух дочерних кластеров
-// p - индекс родителя
-// ll, rr - индекс соседних кластеров
+// left_child, right_child - индексы двух дочерних кластеров
+// size - размер кластера
+// center - центр кластера
 struct Cluster {
-    size_t l, r, p, ll, rr;
+    size_t left_child, right_child, size;
     num center;
-    Cluster(size_t id, size_t ll, size_t rr, const vector<num>& points): l(id), r(id), p(OUT), ll(ll), rr(rr), center(points[id]) {}
-    Cluster(size_t l, size_t r, size_t ll, size_t rr, const vector<Cluster>& clusters): l(l), r(r), p(OUT), ll(ll), rr(rr), center((clusters[l].center + clusters[r].center) / 2) {}
+    Cluster(size_t id, const vector<num>& points):
+        left_child(id), right_child(id), size(1), center(points[id]) {}
+    Cluster(size_t l, size_t r, const vector<Cluster>& clusters):
+        left_child(l), right_child(r), size(clusters[l].size + clusters[r].size),
+        center((clusters[l].center * clusters[l].size + clusters[r].center * clusters[r].size) / (clusters[l].size + clusters[r].size)) {}
+};
+// p - индекс родителя
+// next_left, next_right - индекс соседних кластеров
+struct ClusterInfo {
+    size_t p, next_left, next_right;
+    ClusterInfo(size_t l, size_t r): p(OUT), next_left(l), next_right(r) {}
     bool used() const {
         return p != OUT;
     }
@@ -42,21 +51,24 @@ vector<Cluster> slc(const vector<num>& points) {
         pos[ids[i]] = i;
     }
     vector<Cluster> clusters;
+    vector<ClusterInfo> info;
     clusters.reserve(2 * points.size() - 1);
+    info.reserve(2 * points.size() - 1);
     for (size_t i = 0; i < points.size(); i++) {
         size_t position = pos[i];
-        size_t ll, rr;
+        size_t l, r;
         if (position > 0) {
-            ll = ids[position - 1];
+            l = ids[position - 1];
         } else {
-            ll = OUT;
+            l = OUT;
         }
         if (position + 1 < points.size()) {
-            rr = ids[position + 1];
+            r = ids[position + 1];
         } else {
-            rr = OUT;
+            r = OUT;
         }
-        clusters.push_back(Cluster(i, ll, rr, points));
+        clusters.push_back(Cluster(i, points));
+        info.push_back(ClusterInfo(l, r));
     }
     // Создается куча с парами соседних кластеров
     //
@@ -69,27 +81,27 @@ vector<Cluster> slc(const vector<num>& points) {
         neighbours.push_back(Neighbour{ids[i], ids[i + 1]});
     }
     auto comp = [&clusters](const Neighbour& l, const Neighbour& r) {
-        return clusters[l.r].center - clusters[l.l].center > clusters[r.r].center - clusters[r.l].center;
+        return clusters[l.right].center - clusters[l.left].center > clusters[r.right].center - clusters[r.left].center;
     };
     make_heap(neighbours.begin(), neighbours.end(), comp);
     while (neighbours.size() > 0) {
         Neighbour cur = neighbours.front();
         pop_heap(neighbours.begin(), neighbours.end(), comp);
         neighbours.pop_back();
-        if (!clusters[cur.l].used() && !clusters[cur.r].used()) {
-            clusters.push_back(Cluster(cur.l, cur.r, clusters[cur.l].ll, clusters[cur.r].rr, clusters));
+        if (!info[cur.left].used() && !info[cur.right].used()) {
+            clusters.push_back(Cluster(cur.left, cur.right, clusters));
+            info.push_back(ClusterInfo(info[cur.left].next_left, info[cur.right].next_right));
             size_t new_cluster = clusters.size() - 1;
-            clusters[cur.l].p = new_cluster;
-            clusters[cur.r].p = new_cluster;
-            clusters[clusters[cur.l].ll].rr = new_cluster;
-            clusters[clusters[cur.r].rr].ll = new_cluster;
-
-            if (clusters[cur.l].ll != OUT) {
-                neighbours.push_back(Neighbour{clusters[cur.l].ll, new_cluster});
+            info[cur.left].p = new_cluster;
+            info[cur.right].p = new_cluster;
+            if (info[cur.left].next_left != OUT) {
+                info[info[cur.left].next_left].next_right = new_cluster;
+                neighbours.push_back(Neighbour{info[cur.left].next_left, new_cluster});
                 push_heap(neighbours.begin(), neighbours.end(), comp);
             }
-            if (clusters[cur.r].r != OUT) {
-                neighbours.push_back(Neighbour{new_cluster, clusters[cur.r].rr});
+            if (info[cur.right].next_right != OUT) {
+                info[info[cur.right].next_right].next_left = new_cluster;
+                neighbours.push_back(Neighbour{new_cluster, info[cur.right].next_right});
                 push_heap(neighbours.begin(), neighbours.end(), comp);
             }
         }
@@ -97,10 +109,12 @@ vector<Cluster> slc(const vector<num>& points) {
     return clusters;
 }
 
-/*int main() {
+int main() {
+    std::uniform_real_distribution<double> unif(0, 1);
+    std::default_random_engine re;
     vector<num> vec;
     for (size_t i = 0; i < (1 << 20); i++) {
-        vec.push_back(i);
+        vec.push_back(unif(re));
     }
     auto start = std::chrono::high_resolution_clock::now();
     auto clusters = slc(vec);
@@ -112,54 +126,4 @@ vector<Cluster> slc(const vector<num>& points) {
     //    cout << i << ":  " << clusters[i].l << " " << clusters[i].r << "  " << clusters[i].center << endl;
     //}
     return 0;
-}*/
-
-
-static PyObject *
-example_wrapper(PyObject *dummy, PyObject *args)
-{
-    PyObject *arg1=NULL, *arg2=NULL, *out=NULL;
-    PyObject *arr1=NULL, *arr2=NULL, *oarr=NULL;
-
-    if (!PyArg_ParseTuple(args, "OOO!", &arg1, &arg2,
-        &PyArray_Type, &out)) return NULL;
-
-    arr1 = PyArray_FROM_OTF(arg1, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
-    if (arr1 == NULL) return NULL;
-    arr2 = PyArray_FROM_OTF(arg2, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
-    if (arr2 == NULL) goto fail;
-#if NPY_API_VERSION >= 0x0000000c
-    oarr = PyArray_FROM_OTF(out, NPY_DOUBLE, NPY_ARRAY_INOUT_ARRAY2);
-#else
-    oarr = PyArray_FROM_OTF(out, NPY_DOUBLE, NPY_ARRAY_INOUT_ARRAY);
-#endif
-    if (oarr == NULL) goto fail;
-
-    /* code that makes use of arguments */
-    /* You will probably need at least
-       nd = PyArray_NDIM(<..>)    -- number of dimensions
-       dims = PyArray_DIMS(<..>)  -- npy_intp array of length nd
-                                     showing length in each dim.
-       dptr = (double *)PyArray_DATA(<..>) -- pointer to data.
-
-       If an error occurs goto fail.
-     */
-
-    Py_DECREF(arr1);
-    Py_DECREF(arr2);
-#if NPY_API_VERSION >= 0x0000000c
-    PyArray_ResolveWritebackIfCopy(oarr);
-#endif
-    Py_DECREF(oarr);
-    Py_INCREF(Py_None);
-    return Py_None;
-
- fail:
-    Py_XDECREF(arr1);
-    Py_XDECREF(arr2);
-#if NPY_API_VERSION >= 0x0000000c
-    PyArray_DiscardWritebackIfCopy(oarr);
-#endif
-    Py_XDECREF(oarr);
-    return NULL;
 }
