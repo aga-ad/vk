c26e106167fd7b8e5b420c92ed69fd46808fb9d4
Artur Aganov <aganov.a.d@gmail.com>
2019-05-25 16:20:25 +0300

    done

--- core_ml/main.cpp
+++ core_ml/main.cpp
@@ -7,123 +7,82 @@
 
 using namespace std;
 
-const size_t OUT = (size_t)-1;
+
 typedef double num;
 
-// Содержит индексы двух соседних кластеров
+// Содержит индексы соседних точек
 struct Neighbour {
     size_t left, right;
+    num distance;
+    Neighbour(size_t left, size_t right, num distance):
+        left(left), right(right), distance(distance) {}
 };
 
-// left_child, right_child - индексы двух дочерних кластеров
-// size - размер кластера
-// center - центр кластера
+// Содержит индексы кластеров - потомков
 struct Cluster {
-    size_t left_child, right_child, size;
-    num center;
-    Cluster(size_t id, const vector<num>& points):
-        left_child(id), right_child(id), size(1), center(points[id]) {}
-    Cluster(size_t l, size_t r, const vector<Cluster>& clusters):
-        left_child(l), right_child(r), size(clusters[l].size + clusters[r].size),
-        center((clusters[l].center * clusters[l].size + clusters[r].center * clusters[r].size) / (clusters[l].size + clusters[r].size)) {}
+    size_t left_child, right_child;
+    Cluster (size_t id):
+        left_child(id), right_child(id) {}
+    Cluster(size_t left_child, size_t right_child):
+        left_child(left_child), right_child(right_child) {}
 };
-// p - индекс родителя
-// next_left, next_right - индекс соседних кластеров
+
+// Содержит индекс корневого кластера и индексы самой левой и правой точкек кластера
 struct ClusterInfo {
-    size_t p, next_left, next_right;
-    ClusterInfo(size_t l, size_t r): p(OUT), next_left(l), next_right(r) {}
-    bool used() const {
-        return p != OUT;
-    }
+    size_t root, left_bound, right_bound;
+    ClusterInfo(size_t id):
+        root(id), left_bound(id), right_bound(id) {}
 };
 
 vector<Cluster> slc(const vector<num>& points) {
-    // Cоздается вектор кластеров, состоящих из одного элемента
-    // ids[k] - k-порядковая статистика, pos[k] - позиция в отсортированном списке кластеров;
-    // ids, pos используются для эффективного поиска соседей для каждого единичного кластера
+    // Сортируем индексы точек по возрастанию,
+    // Получаем пары соседних точек, сортируем их по возрастанию расстояния
+    vector<Cluster> clusters;
+    vector<ClusterInfo> info;
+    clusters.reserve(2 * points.size() - 1);
+    info.reserve(points.size());
+    for (size_t i = 0; i < points.size(); i++) {
+        clusters.push_back(Cluster(i));
+        info.push_back(ClusterInfo(i));
+    }
     vector<size_t> ids(points.size());
     for (size_t i = 0; i < ids.size(); i++) {
         ids[i] = i;
     }
     sort(ids.begin(), ids.end(), [&points](size_t a, size_t b){return points[a] < points[b];});
-    vector<size_t> pos(points.size());
-    for (size_t i = 0; i < ids.size(); i++) {
-        pos[ids[i]] = i;
-    }
-    vector<Cluster> clusters;
-    vector<ClusterInfo> info;
-    clusters.reserve(2 * points.size() - 1);
-    info.reserve(2 * points.size() - 1);
-    for (size_t i = 0; i < points.size(); i++) {
-        size_t position = pos[i];
-        size_t l, r;
-        if (position > 0) {
-            l = ids[position - 1];
-        } else {
-            l = OUT;
-        }
-        if (position + 1 < points.size()) {
-            r = ids[position + 1];
-        } else {
-            r = OUT;
-        }
-        clusters.push_back(Cluster(i, points));
-        info.push_back(ClusterInfo(l, r));
-    }
-    // Создается куча с парами соседних кластеров
-    //
-    // Используя кучу, достаем пару ближайших кластеров,
-    // если каждый из них не был объединен, то объединяем их в новый кластер,
-    // добавляем две новые пары соседних кластеров
     vector<Neighbour> neighbours;
     neighbours.reserve(ids.size() - 1);
-    for (size_t i = 0; i + 1 < ids.size(); i++) {
-        neighbours.push_back(Neighbour{ids[i], ids[i + 1]});
+    for (size_t i = 1; i < ids.size(); i++) {
+        neighbours.push_back(Neighbour{ids[i - 1], ids[i], points[ids[i]] - points[ids[i - 1]]});
     }
-    auto comp = [&clusters](const Neighbour& l, const Neighbour& r) {
-        return clusters[l.right].center - clusters[l.left].center > clusters[r.right].center - clusters[r.left].center;
-    };
-    make_heap(neighbours.begin(), neighbours.end(), comp);
-    while (neighbours.size() > 0) {
-        Neighbour cur = neighbours.front();
-        pop_heap(neighbours.begin(), neighbours.end(), comp);
-        neighbours.pop_back();
-        if (!info[cur.left].used() && !info[cur.right].used()) {
-            clusters.push_back(Cluster(cur.left, cur.right, clusters));
-            info.push_back(ClusterInfo(info[cur.left].next_left, info[cur.right].next_right));
-            size_t new_cluster = clusters.size() - 1;
-            info[cur.left].p = new_cluster;
-            info[cur.right].p = new_cluster;
-            if (info[cur.left].next_left != OUT) {
-                info[info[cur.left].next_left].next_right = new_cluster;
-                neighbours.push_back(Neighbour{info[cur.left].next_left, new_cluster});
-                push_heap(neighbours.begin(), neighbours.end(), comp);
-            }
-            if (info[cur.right].next_right != OUT) {
-                info[info[cur.right].next_right].next_left = new_cluster;
-                neighbours.push_back(Neighbour{new_cluster, info[cur.right].next_right});
-                push_heap(neighbours.begin(), neighbours.end(), comp);
-            }
-        }
+    sort(neighbours.begin(), neighbours.end(), [](const Neighbour& a, const Neighbour& b){return a.distance < b.distance;});
+    // Объединяем кластеры с минимальным расстоянием, поддерживаем info
+    for (size_t i = 0; i < neighbours.size(); i++) {
+        size_t l = neighbours[i].left;
+        size_t r = neighbours[i].right;
+        size_t new_cluster = clusters.size();
+        clusters.push_back(Cluster(info[l].root, info[r].root));
+        info[info[l].left_bound].root = new_cluster;
+        info[info[r].right_bound].root = new_cluster;
+        info[info[l].left_bound].right_bound = info[r].right_bound;
+        info[info[r].right_bound].left_bound = info[l].left_bound;
     }
     return clusters;
 }
 
 int main() {
-    std::uniform_real_distribution<double> unif(0, 1);
-    std::default_random_engine re;
     vector<num> vec;
-    for (size_t i = 0; i < (1 << 20); i++) {
-        vec.push_back(unif(re));
+    num t;
+    while (cin >> t) {
+        vec.push_back(t);
     }
-    auto start = std::chrono::high_resolution_clock::now();
     auto clusters = slc(vec);
-    auto end = std::chrono::high_resolution_clock::now();
-    auto time_taken_1 = end - start;
-    std::cout << "slc took "
-    << std::chrono::duration_cast<std::chrono::milliseconds>(time_taken_1).count() << " ms\n";
-    //for (size_t i = 0; i < clusters.size(); i++) {
-    //    cout << i << ":  " << clusters[i].l << " " << clusters[i].r << "  " << clusters[i].center << endl;
-    //}
+    for (size_t i = 0; i < clusters.size(); i++) {
+        if (i < vec.size()) {
+            cout << i << ": " << vec[i] << endl;
+        } else {
+            cout << i << ":  " << clusters[i].left_child << " " << clusters[i].right_child  << endl;
+        }
+    }
     return 0;
 }
